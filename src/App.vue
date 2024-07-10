6c7cfd14f8d85402a4eac3b65fd9b2d40aafa092
Lourenço Carlos <reincedaniel@gmail.com>
2024-07-10 12:23:56 +0100

    refactor: page one

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,28 @@
 
 <script setup>
 defineOptions({
-  name: 'App'
+  name: "App",
 });
 </script>
+
+<style lang="scss">
+body::-webkit-scrollbar {
+  width: 4px;
+}
+
+body::-webkit-scrollbar-thumb {
+  background: rgba(131, 58, 180, 0.678);
+  background: linear-gradient(
+    30deg,
+    rgba(131, 58, 180, 1) 0%,
+    rgba(242, 62, 62, 0.692) 35%,
+    rgba(252, 176, 69, 0.692) 100%
+  );
+  border-radius: 10px;
+}
+
+.curve-surve {
+  border-bottom-right-radius: 10px;
+  border-top-left-radius: 10px;
+}
+</style>
